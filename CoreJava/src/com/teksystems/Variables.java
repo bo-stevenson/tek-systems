3f9f3777b96424fb849e3943e10ff8b77a681542
Bo Stevenson <bostevenson1027@gmail.com>
2022-03-01 13:31:50 -0600

    added work from slides

--- CoreJava/src/com/teksystems/Variables.java
+++ CoreJava/src/com/teksystems/Variables.java
@@ -1,83 +1,64 @@
 package com.teksystems;
 
-import java.util.Scanner;
-
 public class Variables {
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter weight in kilograms: ");
-        double weight = scan.nextDouble();
-        System.out.println("Enter height in meters: ");
-        double height = scan.nextDouble();
-        double BMI = weight / (height * height);
-        if (BMI < 18.5D) {
-            System.out.println("Underweight");
-        } else if (BMI >= 18.5D && BMI < 25.0D) {
-            System.out.println("Normal");
-        } else if (BMI >= 25.0D && BMI < 30.0D) {
-            System.out.println("Overweight");
-        } else {
-            System.out.println("Obese");
-        }
 
-        System.out.println("The BMI is " + BMI + " kg/m2");
-        int zyear = 2022;
-        switch (zyear % 12) {
-            case 0:
-                System.out.println("monkey");
-                break;
-            case 1:
-                System.out.println("rooster");
-                break;
-            case 2:
-                System.out.println("dog");
-                break;
-            case 3:
-                System.out.println("pig");
-                break;
-            case 4:
-                System.out.println("rat");
-                break;
-            case 5:
-                System.out.println("ox");
-                break;
-            case 6:
-                System.out.println("tiger");
-                break;
-            case 7:
-                System.out.println("rabbit");
-                break;
-            case 8:
-                System.out.println("dragon");
-                break;
-            case 9:
-                System.out.println("snake");
-                break;
-            case 10:
-                System.out.println("horse");
-                break;
-            case 11:
-                System.out.println("sheep");
-        }
+        //1
+        int var1 = 6;
+        int var2 = 7;
+        int sum = var1 + var2;
+        System.out.println(sum);
 
-        int tableSize = 12;
+        //2
+        double d1 = 4.5;
+        double d2 = 2.4;
+        double dSum = d1 + d2;
+        System.out.println(dSum);
 
-        int year;
-        for (year = 1; year <= tableSize; ++year) {
-            for (int j = 1; j <= tableSize; ++j) {
-                System.out.printf("%4d", year * j);
-            }
+        //3
+        double a = 4.3;
+        int b = 5;
+        double c = a + b;
+        System.out.println(c);
 
-            System.out.println();
-        }
+        //4
+        double big = 60.5;
+        double small = 5;
+        double quotient = big / small;
+        System.out.println(quotient);
+        int value = (int) quotient;
+        System.out.println(value);
 
-        year = 0;
+        //5
+        int x = 5;
+        int y = 6;
+        double q = 0;
+        q = y / x;
+        System.out.println(q);
+        double z = y;
+        q = (z / x);
+        System.out.println(q);
 
-        for (double tuition = 10000.0D; tuition < 20000.0D; ++year) {
-            System.out.printf(year + " %2f \n", tuition);
-            tuition = 1.07D * tuition;
-        }
+        //6
+        final double Pi = 3.14;
+        double radius = 25;
+        double area = (Pi * radius * radius);
+        System.out.println(area);
 
-        System.out.println("In " + year + " years tuition will be doubled");
+        //7
+        double coffee = 4.50;
+        double espresso = 6.75;
+        double boba = 8;
+
+        double subtotal = 3 * coffee + 4 * espresso + 2 * boba;
+        System.out.println("Subtotal is: " + subtotal);
+        final double SALES_TAX = 0.15;
+        double totalSale = subtotal - (SALES_TAX * subtotal);
+        System.out.println("Total Sale is: " + totalSale);
+
+
+
+
     }
 }
---
+++ CoreJava/src/com/teksystems/zodiacBMI.java
@@ -0,0 +1,83 @@
+package com.teksystems;
+
+import java.util.Scanner;
+
+public class zodiacBMI {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter weight in kilograms: ");
+        double weight = scan.nextDouble();
+        System.out.println("Enter height in meters: ");
+        double height = scan.nextDouble();
+        double BMI = weight / (height * height);
+        if (BMI < 18.5D) {
+            System.out.println("Underweight");
+        } else if (BMI >= 18.5D && BMI < 25.0D) {
+            System.out.println("Normal");
+        } else if (BMI >= 25.0D && BMI < 30.0D) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
+        }
+
+        System.out.println("The BMI is " + BMI + " kg/m2");
+        int zyear = 2022;
+        switch (zyear % 12) {
+            case 0:
+                System.out.println("monkey");
+                break;
+            case 1:
+                System.out.println("rooster");
+                break;
+            case 2:
+                System.out.println("dog");
+                break;
+            case 3:
+                System.out.println("pig");
+                break;
+            case 4:
+                System.out.println("rat");
+                break;
+            case 5:
+                System.out.println("ox");
+                break;
+            case 6:
+                System.out.println("tiger");
+                break;
+            case 7:
+                System.out.println("rabbit");
+                break;
+            case 8:
+                System.out.println("dragon");
+                break;
+            case 9:
+                System.out.println("snake");
+                break;
+            case 10:
+                System.out.println("horse");
+                break;
+            case 11:
+                System.out.println("sheep");
+        }
+
+        int tableSize = 12;
+
+        int year;
+        for (year = 1; year <= tableSize; ++year) {
+            for (int j = 1; j <= tableSize; ++j) {
+                System.out.printf("%4d", year * j);
+            }
+
+            System.out.println();
+        }
+
+        year = 0;
+
+        for (double tuition = 10000.0D; tuition < 20000.0D; ++year) {
+            System.out.printf(year + " %2f \n", tuition);
+            tuition = 1.07D * tuition;
+        }
+
+        System.out.println("In " + year + " years tuition will be doubled");
+    }
+}
